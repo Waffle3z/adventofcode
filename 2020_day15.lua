fa5d41d40ea71da7be6f66d185e1a847c0c8dcbd
Waffle3z <w4ffle3z@gmail.com>
2020-12-15 00:18:42 -0500

    Create 2020_day15.lua

---
+++ 2020_day15.lua
@@ -0,0 +1,20 @@
+local t = {}
+for v in input:gmatch("[^,]+") do
+	t[#t+1] = tonumber(v)
+end
+local last = {}
+for i=1,#t-1 do last[t[i]] = i end
+local lastn, lastv = t[#t], #t
+for i = #t+1, 30000000 do
+	local n = t[#t]
+	if last[n] and last[n] ~= #t then
+		t[#t+1] = #t - last[n]
+	else
+		t[#t+1] = 0
+	end
+	if lastn then last[lastn] = lastv end
+	lastn, lastv = t[#t], #t
+	if i%1e5 == 0 then print("-"..(i/1e6)) end
+end
+print(t[2020])
+print(t[#t])
